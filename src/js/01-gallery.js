5671762dc6ac3343f6cd53cfae89e0e40d020369
iullian1919 <127934021+iullian1919@users.noreply.github.com>
2024-01-31 20:33:54 +0200

    AddGallery commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+listEl.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+let lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createMarkup(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>`;
+    })
+    .join('');
+}
