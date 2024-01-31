e81e4481dfd88188ff2df4ce83cdcef2ac0a1854
iullian1919 <127934021+iullian1919@users.noreply.github.com>
2024-01-31 22:03:16 +0200

    AddVideo commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+if (localStorage.getItem('videoplayer-current-time')) {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+function saveTime(currentTime) {
+  localStorage.setItem('videoplayer-current-time', currentTime.seconds);
+}
