246b3295e867a9a11ab42d90c12a4cc0f25c2dd5
iullian1919 <127934021+iullian1919@users.noreply.github.com>
2024-02-01 10:32:36 +0200

    AddFeedback commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const dataObj = { email: '', message: '' };
+
+checkStorage(localStorage, formEl);
+
+formEl.addEventListener('input', throttle(inputsListener, 500));
+formEl.addEventListener('submit', formSubmit);
+
+function inputsListener(ev) {
+  if (ev.target.name === 'email') {
+    dataObj.email = ev.target.value;
+  } else dataObj.message = ev.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataObj));
+}
+
+function checkStorage(dataValue, form) {
+  const dataObj = dataValue.getItem('feedback-form-state');
+  if (dataObj) {
+    const dataObjParsed = JSON.parse(dataObj);
+    if (dataObjParsed.email) {
+      form.elements.email.value = dataObjParsed.email;
+    }
+    if (dataObjParsed.message) {
+      form.elements.message.value = dataObjParsed.message;
+    }
+  }
+}
+
+function formSubmit(ev) {
+  ev.preventDefault();
+  console.dir(dataObj);
+  ev.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
